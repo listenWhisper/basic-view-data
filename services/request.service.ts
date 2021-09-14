8a30a3b96a3a6869c69c9fa913511781211043b8
listen-whisper <listen-whisper@outlook.com>
2021-09-14 17:14:09 +0800

    bug(*): 引用声明

--- services/request.service.ts
+++ services/request.service.ts
@@ -2,65 +2,68 @@ import {ErrorResponse} from '../domains/error-response.domain';
 import {RequestTypeEnum} from '../enums';
 import {EnumType, UtilTools} from '../share';
 // @ts-ignore
-import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 // @ts-ignore
-import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
 
 @Injectable({
     providedIn: 'root',
 })
-export class RequestService{
-    private _proxy = ""; // 代理地址
-    private _requestType: EnumType<RequestTypeEnum> = RequestTypeEnum.angular; // 请求方式 根据不同环境 不同设置
-    private _contentType: Record<string, any> = { 'content-type': 'application/x-www-form-urlencoded' } // 默认请求头
-
+export class RequestService {
     constructor(
         private http: HttpClient
     ) {
     }
 
-    public get requestType(){
-        return this._requestType;
+    private _proxy = ''; // 代理地址
+
+    public get proxy() {
+        return this._proxy;
     }
 
-    public set requestType(requestType: EnumType<RequestTypeEnum>){
-        this._requestType = requestType;
-    }
-
-    public get contentType(){
-        return this._contentType;
-    }
-
-    public set contentType(contentType: Record<string, any>){
-        this._contentType = contentType;
-    }
-
-    public get proxy(){
-       return this._proxy;
-    }
-
-    public set proxy(proxy: string){
+    public set proxy(proxy: string) {
         this._proxy = proxy;
     }
 
-    public get<T>(url: string, data: Record<string, any> = {}, header: Record<string, any> = {}){
+    private _requestType: EnumType<RequestTypeEnum> = RequestTypeEnum.angular; // 请求方式 根据不同环境 不同设置
+
+    public get requestType() {
+        return this._requestType;
+    }
+
+    public set requestType(requestType: EnumType<RequestTypeEnum>) {
+        this._requestType = requestType;
+    }
+
+    private _contentType: Record<string, any> = {'content-type': 'application/x-www-form-urlencoded'}; // 默认请求头
+
+    public get contentType() {
+        return this._contentType;
+    }
+
+    public set contentType(contentType: Record<string, any>) {
+        this._contentType = contentType;
+    }
+
+    public get<T>(url: string, data: Record<string, any> = {}, header: Record<string, any> = {}) {
         url = UtilTools.timestamp(url);
         return this.requestOption<T>({
             method: 'GET',
             url,
             header: Object.assign(this.contentType, header),
             data
-        })
+        });
     }
 
-    public post<T>(url: string, data: Record<string, any> = {}, header: Record<string, any> = {}){
+    public post<T>(url: string, data: Record<string, any> = {}, header: Record<string, any> = {}) {
         url = UtilTools.timestamp(url);
         return this.requestOption<T>({
             method: 'POST',
             url,
             header: Object.assign(this.contentType, header),
             data
-        },)
+        },);
     }
 
     private requestOption<T>(options: Record<string, any> = {}): Promise<any> {
@@ -69,25 +72,25 @@ export class RequestService{
             reject: (err: { Code?: string; Msg?: string; Stacks?: string }) => void
         ) => {
             options.success = (response: any) => {
-                if(response.statusCode !== 200){
+                if (response.statusCode !== 200) {
                     resolve(response);
                 }
-                if(typeof response.data === 'object'){
-                    if(response.data.hasOwnProperty('error_response')){
+                if (typeof response.data === 'object') {
+                    if (response.data.hasOwnProperty('error_response')) {
                         response.data = new ErrorResponse(response.data.error_response);
                         reject(response);
                     }
                     resolve(response);
                 }
             };
-            this.request(options)
-        })
+            this.request(options);
+        });
     }
 
-    private request(options: Record<string, any> ){
+    private request(options: Record<string, any>) {
         switch (this.requestType) {
             case RequestTypeEnum.angular:
-                this.angularRequest(options)
+                this.angularRequest(options);
                 break;
             case RequestTypeEnum.uni:
                 this.uniRequest(options);
@@ -103,24 +106,24 @@ export class RequestService{
         }
     }
 
-    private angularRequest(options: Record<string, any>){
+    private angularRequest(options: Record<string, any>) {
         this.http.post(options?.url, options?.data, options?.header).subscribe((res: any) => {
             options?.success(res);
-        })
+        });
     }
 
 
-    private uniRequest(options: Record<string, any>){
+    private uniRequest(options: Record<string, any>) {
         // @ts-ignore
         uni.request(options);
     }
 
-    private wxRequest(options: Record<string, any>){
+    private wxRequest(options: Record<string, any>) {
         // @ts-ignore
         wx.request(options);
     }
 
-    private vueRequest(options: Record<string, any>){
+    private vueRequest(options: Record<string, any>) {
 
     }
 }
