728030eb86cf60e96a2ddc3cfc2e87d8a12e9b34
listen-whisper <listen-whisper@outlook.com>
2021-09-14 17:26:06 +0800

    bug(*): 引用声明

--- services/request.service.ts
+++ services/request.service.ts
@@ -20,6 +20,16 @@ export class RequestService {
         this._proxy = proxy;
     }
 
+    private _func: any = () => {}; // 代理地址
+
+    public get func() {
+        return this._func;
+    }
+
+    public set func(func: () => {}) {
+        this._func = func;
+    }
+
     private _requestType: EnumType<RequestTypeEnum> = RequestTypeEnum.angular; // 请求方式 根据不同环境 不同设置
 
     public get requestType() {
@@ -82,22 +92,7 @@ export class RequestService {
     }
 
     private request(options: Record<string, any>) {
-        switch (this.requestType) {
-            case RequestTypeEnum.angular:
-                this.angularRequest(options);
-                break;
-            case RequestTypeEnum.uni:
-                this.uniRequest(options);
-                break;
-            case RequestTypeEnum.wx:
-                this.wxRequest(options);
-                break;
-            case RequestTypeEnum.vue:
-                this.vueRequest(options);
-                break;
-            default:
-                throw new Error('未找到定向请求,请更改请求类型');
-        }
+        this._func(options);
     }
 
     private angularRequest(options: Record<string, any>) {
